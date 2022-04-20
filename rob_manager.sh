f02ea7ba6ecabc974a728a9d999df4293eae1a4a
inspirers <arvid.boisen@protonmail.com>
2022-04-20 13:29:51 +0200

    Added controller.py for looking for controller

--- rob_manager.sh
+++ rob_manager.sh
@@ -1,6 +1,7 @@
-cd /home/pi/Colinear-Balance-Bot/
+#!/bin/bash
 
-#sudo python3 -c "from indicator import *; indicate(0, (0, 255, 0))"
-#sudo ds4drv >> ds4drv.log 2>&1 &
+#cd /home/pi/Colinear-Balance-Bot/ #Not required if executing with ./rob_manager.sh
+
+sudo python3 controller.py #Checks if controller is connected. Closes when connected
 sudo python3 main.py #Has to be executed as sudo because odrive requires it to.
 
